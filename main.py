1bb8743505a1920f3cc434c54c44e3a9787896e0
zoecjy <111746852+zoecjy@users.noreply.github.com>
2024-10-29 10:03:41 +0800

    Update main.py with correct bolding for disclaimer

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def about_us_page():
 # Show the modal if it's supposed to be shown
 if st.session_state.show_modal:
     with st.expander("Welcome to Checkmate!", expanded=True):
-        st.write("⚠️ **IMPORTANT NOTICE** ⚠️: This web application is developed as a proof-of-concept prototype. The information provided here is **NOT** intended for actual usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters. Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output. Always consult with qualified professionals for accurate and personalised advice.")
+        st.write("⚠️ **IMPORTANT NOTICE** ⚠️: This web application is developed as a proof-of-concept prototype. The information provided here is **NOT intended for actual usage** and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters. **Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.** Always consult with qualified professionals for accurate and personalised advice.")
         if st.button("I Agree"):
             st.session_state.show_modal = False  # Close the modal
 
